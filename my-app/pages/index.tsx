c7bbebfd7375d5edd238745b914b9c7a7c186a55
Aarav Agarwal <aaravagarwal.1511@gmail.com>
2022-04-07 19:34:02 +0530

    pre-checked 2

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -35,7 +35,7 @@ const Home: NextPage = () => {
       }
     >
       <Center className="mb-10">
-        <Image src="/image12.jpeg" width="1500" height="300" className='rounded-lg' />
+        <Image src="/image12.jpeg" width="1500" height="300" className='rounded-lg' alt="" />
       </Center>
       <Tabs>
       <Tabs.Tab label="Poverty Introduction">
@@ -47,14 +47,14 @@ const Home: NextPage = () => {
 
           <Accordion.Item label="What is Poverty?">
             <SimpleGrid cols={3}>
-              <Image src="/image4.jpeg" width="600" height="400" className="rounded-lg" />
+              <Image src="/image4.jpeg" width="600" height="400" className="rounded-lg" alt="" />
               <p className="text-4xl">Poverty is when a person cannot afford their basic human needs like food,water etc.</p>
             </SimpleGrid>
             
           </Accordion.Item>
           <Accordion.Item label="What are the types of poverty?">
           <SimpleGrid cols={3}>
-            <Image src="/image5.jpeg" width="500" height="500" className="rounded-lg" />
+            <Image src="/image5.jpeg" width="500" height="500" className="rounded-lg" alt="" />
               <p className="text-4xl">There are 3 types of poverty, each one worse than the last. Relative, Absolute & Extreme</p>
             </SimpleGrid>
 
@@ -79,7 +79,7 @@ const Home: NextPage = () => {
           </Accordion.Item>
           <Accordion.Item label="What is the main cause of poverty?">
             <SimpleGrid cols={3}>
-              <Image src="/image6.jpeg" width="500" height="500" className="rounded-lg" />
+              <Image src="/image6.jpeg" width="500" height="500" className="rounded-lg" alt="" />
               <p className="text-4xl">Inequality and Discrimination</p>
             </SimpleGrid>
           </Accordion.Item>
@@ -92,25 +92,25 @@ const Home: NextPage = () => {
         }}>
       <Accordion.Item label="Cause Inequality">
         <SimpleGrid cols={3}>
-          <Image src="/image7.jpeg" width="300" height="350" className="rounded-lg" />
+          <Image src="/image7.jpeg" width="300" height="350" className="rounded-lg" alt="" />
           <p className="text-4xl">Inequality of Income can lead to less money, which of course leads to Poverty.</p>
         </SimpleGrid>
       </Accordion.Item>
       <Accordion.Item label="Cause Wars">
         <SimpleGrid cols={3}>
-        <Image src="/image8.jpeg" width="300" height="350" className="rounded-lg" />  
+        <Image src="/image8.jpeg" width="300" height="350" className="rounded-lg" alt="" />  
         <p className="text-4xl">Wars have more than one missile. So with your money blown up, e-cash databases are blown up. Stocks Going Down, like The Great Depression. It can also ruin, or destroy resources.</p>
         </SimpleGrid>
       </Accordion.Item>
       <Accordion.Item label="Cause Migration Issues">
         <SimpleGrid cols={3}>
-          <Image src="/image9.jpeg" width="300" height="350" className="rounded-lg" />  
+          <Image src="/image9.jpeg" width="300" height="350" className="rounded-lg" alt="" />  
           <p className="text-4xl">Migration issues can also escalate poverty and hunger as the refugees might not have jobs in different countries where they migrate.</p>
         </SimpleGrid>
       </Accordion.Item>
       <Accordion.Item label="Cause Unemployment">
         <SimpleGrid cols={3}>
-          <Image src="/image10.jpeg" width="300" height="350" className="rounded-lg" />  
+          <Image src="/image10.jpeg" width="300" height="350" className="rounded-lg" alt="" />  
           <p className="text-4xl">Unemployment means no money, and no money means you will, gradually, shift to Poverty.</p>
         </SimpleGrid>
       </Accordion.Item>
@@ -132,7 +132,7 @@ const Home: NextPage = () => {
             <Title>Oprah Winfrey</Title>
             <p className="ml-1 mt-5">Oprah Winfrey is a Americian Talk show host who was the richest Black/Africian-Americian Billionare in the 20th Century. She was born in a poor single teenage mother which lived in Mississppi. Oprah got a child in the age of 15 who died pretty early in life. Then Oprah was sent to a person she called her father. His name was Vernon Winfrey, a barber in Nashville, Tennessee. She then went to High School, and still while in High School landed a job. By 19 She was a Co-anchor for the Local Evening News. In 2002 Oprah Winfrey and South Africian President Nelson Mandela were talking about how to combat Poverty, Oprah Winfrey though about making a school for Girls. 528 Girls Graduated from OWLAG (The Oprah Winfrey Leadership Academy for Girls). 90% of Girls who attend Collage after graduating from OWLAG pass.</p>
           </div>
-          <Image src="/image1.jpeg" width="300" height="450"/>
+          <Image src="/image1.jpeg" width="300" height="450" alt="" />
         </SimpleGrid>
         <br /><hr /><br />
         <SimpleGrid cols={2}>
@@ -140,7 +140,7 @@ const Home: NextPage = () => {
             <Title>Ban Ki-moon</Title>
             <p className="ml-1 mt-5">Born June 13th 1944 is a South Korean politician and diplomat who had served eigth secretery general (2007-16) of the U.N (United Nations). At age 18 Ban had won a competition that took him to the White House to meet U.S President John F. Kennedy, a visit that Ban claimed most inspired his public career. He received a bachelor’s degree in 1970 on international relations from Seoul National University and earned a master’s degree in 1985 from the John F. Kennedy School of Government at Harvard University. He is one of the people that helped creating the SDG goals to make a better world.</p>
           </div>
-          <Image src="/image2.jpeg" width="300" height="370"/>
+          <Image src="/image2.jpeg" width="300" height="370" alt="" />
         </SimpleGrid>
       </Tabs.Tab>
       <Tabs.Tab label="GDP">
@@ -171,7 +171,7 @@ If an old house is resold, it's not counted in the GDP since it was not original
 The part ‘within a country’ confuses a lot of people, a simple definition is basically when USA imports something from france, it adds to France's GDP, and if France imports something from USA, it adds to USA’s GDP
 </p>
           </div>
-          <Image src="/image11.png" width="300" height="450"/>
+          <Image src="/image11.png" width="300" height="450" alt="" />
         </SimpleGrid>
       </Tabs.Tab>
       <Tabs.Tab label="3 Theories">
